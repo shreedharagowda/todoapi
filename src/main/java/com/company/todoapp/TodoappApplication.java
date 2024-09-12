388e30785766ff580ab0371fb14c50df949ab2e5
vijay <vijay.nvb@gmail.com>
2024-09-12 14:35:09 +0530

    Update TodoappApplication.java

--- src/main/java/com/company/todoapp/TodoappApplication.java
+++ src/main/java/com/company/todoapp/TodoappApplication.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+SpringBootApplication
 @SecurityScheme(name = "TodoApp", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 
 public class TodoappApplication {
